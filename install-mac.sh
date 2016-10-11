abd666843a4996d556f92b606343e9d9b5bb5f17
Albert Bori <github@albertbori.com>
2016-10-11 00:26:13 -0600

    Added symlink option to installer (for contributor installs)

--- install-mac.sh
+++ install-mac.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DEFAULT_DIRECTORY="/Applications/World of Warcraft"
+USE_SYMLINKS=false
 
 if [ ! -z "$0" ] && [[ "$TARGET_DIRECTORY" =~ "World of Warcraft$" ]] && [ -d "$TARGET_DIRECTORY" ] ; then
     TARGET_DIRECTORY="$0"
@@ -9,7 +10,12 @@ elif [ -d "$DEFAULT_DIRECTORY" ]; then
     TARGET_DIRECTORY="$DEFAULT_DIRECTORY"
 fi
 
-echo "To install this, we have to get enough information to target the correct WoW folders. This includes providing your account name, realm name and main character's name.\n"
+if [[ "$@" == "-link" ]] ; then
+    echo "Symlink mode enabled..."
+    USE_SYMLINKS=true
+fi
+
+echo "---\nTo install this, we have to get enough information to target the correct WoW folders. This includes providing your account name, realm name and main character's name.\n---"
 
 function promptInfo {
     read -p "Enter your WoW Account name (case sensitive): " ACCOUNT_NAME
@@ -62,21 +68,38 @@ function install {
     if [ -d "$TARGET_DIRECTORY/Interface" ] && [ ! -d "$TARGET_DIRECTORY/Interface_Backup" ] ; then
         echo "Backing up '$TARGET_DIRECTORY/Interface' folder..."
         cp -r "$TARGET_DIRECTORY/Interface" "$TARGET_DIRECTORY/Interface_Backup"
+    else
+        echo "Already backed up '$TARGET_DIRECTORY/Interface' folder."
     fi
 
     if [ -d "$TARGET_DIRECTORY/WTF" ] && [ ! -d "$TARGET_DIRECTORY/WTF_Backup" ] ; then
         echo "Backing up '$TARGET_DIRECTORY/WTF' folder..."
         cp -r "$TARGET_DIRECTORY/WTF" "$TARGET_DIRECTORY/WTF_Backup"
+    else
+        echo "Already backed up '$TARGET_DIRECTORY/WTF' folder."
     fi
 
-    echo "Installing addons..."
-    cp -rf "$SCRIPT_DIRECTORY/Interface" "$TARGET_DIRECTORY/Interface"
+    if [ "$USE_SYMLINKS" = true ] ; then
+        echo "Setting up addons symlink..."
+        rm -rf "$TARGET_DIRECTORY/Interface"
+        ln -s "$SCRIPT_DIRECTORY/Interface" "$TARGET_DIRECTORY/Interface"
 
-    echo "Installing profiles..."
-    mkdir -p "$TARGET_DIRECTORY/WTF/Account/$ACCOUNT_NAME/$SERVER_NAME/$CHARACTER_NAME"
-    cp -rf "$SCRIPT_DIRECTORY/WTF/Account/[ACCOUNTNAME]/[SERVERNAME]/[CHARACTERNAME]/SavedVariables" "$TARGET_DIRECTORY/WTF/Account/$ACCOUNT_NAME/$SERVER_NAME/$CHARACTER_NAME/SavedVariables"
-    mkdir -p "$TARGET_DIRECTORY/WTF/Account/$ACCOUNT_NAME"
-    cp -rf "$SCRIPT_DIRECTORY/WTF/Account/[ACCOUNTNAME]/SavedVariables" "$TARGET_DIRECTORY/WTF/Account/$ACCOUNT_NAME/SavedVariables"
+        echo "Setting up profiles symlink..."
+        rm -rf "$TARGET_DIRECTORY/WTF"
+        mkdir -p "$TARGET_DIRECTORY/WTF/Account/$ACCOUNT_NAME/$SERVER_NAME/$CHARACTER_NAME"
+        ln -s "$SCRIPT_DIRECTORY/WTF/Account/[ACCOUNTNAME]/[SERVERNAME]/[CHARACTERNAME]/SavedVariables" "$TARGET_DIRECTORY/WTF/Account/$ACCOUNT_NAME/$SERVER_NAME/$CHARACTER_NAME/SavedVariables"
+        mkdir -p "$TARGET_DIRECTORY/WTF/Account/$ACCOUNT_NAME"
+        ln -s "$SCRIPT_DIRECTORY/WTF/Account/[ACCOUNTNAME]/SavedVariables" "$TARGET_DIRECTORY/WTF/Account/$ACCOUNT_NAME/SavedVariables" 
+    else
+        echo "Installing addons..."
+        cp -rf "$SCRIPT_DIRECTORY/Interface" "$TARGET_DIRECTORY/Interface"
+        
+        echo "Installing profiles..."
+        mkdir -p "$TARGET_DIRECTORY/WTF/Account/$ACCOUNT_NAME/$SERVER_NAME/$CHARACTER_NAME"
+        cp -rf "$SCRIPT_DIRECTORY/WTF/Account/[ACCOUNTNAME]/[SERVERNAME]/[CHARACTERNAME]/SavedVariables" "$TARGET_DIRECTORY/WTF/Account/$ACCOUNT_NAME/$SERVER_NAME/$CHARACTER_NAME/SavedVariables"
+        mkdir -p "$TARGET_DIRECTORY/WTF/Account/$ACCOUNT_NAME"
+        cp -rf "$SCRIPT_DIRECTORY/WTF/Account/[ACCOUNTNAME]/SavedVariables" "$TARGET_DIRECTORY/WTF/Account/$ACCOUNT_NAME/SavedVariables"        
+    fi
 
     echo "Installation complete."
     exit
